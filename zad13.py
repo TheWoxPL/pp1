49b3cedfff81878e44c0ee70bfcb81b98ee6b0f8
TheWoxPL <wojtek.bubula@onet.pl>
2022-10-10 23:32:35 +0200

    Finished 2 chapter

---
+++ zad13.py
@@ -0,0 +1,6 @@
+import math
+radius=5
+PI=math.pi
+area=PI*radius**2
+circumference=2*PI*radius
+print(f"Area: {area}, circumference: {circumference}")
---
+++ zad14.py
@@ -0,0 +1,4 @@
+temp_celsius=int(input("Write temperature in Celsius: "))
+temp_kelvin=temp_celsius+273.15
+temp_fahrenheit=temp_celsius*1.8+32
+print(f"Celsius: {temp_celsius}, Kelvin: {temp_kelvin}, Fahrenheit: {temp_fahrenheit}")
