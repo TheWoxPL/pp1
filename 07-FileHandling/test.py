b24664fb1165076663059143edee8c2d3e4646e2
TheWoxPL <wojtek.bubula@onet.pl>
2022-11-28 12:02:25 +0100

    Testing before class

---
+++ 07-FileHandling/test.py
@@ -0,0 +1,26 @@
+# from collections import deque
+
+# stack = []
+# stack.append(1)
+# stack.append(2)
+# stack.append(3)
+
+# print(stack)
+
+# print(stack.pop())
+# print(stack.pop())
+# print(stack.pop())
+
+# print(stack)
+
+# import json
+# with open("generated.json", "r") as file:
+#     new_file=json.loads(file.read())
+#     print(new_file[0]['_id'])
+
+dict={
+    "name":"Wojtek",
+    "surname":"Bubula"
+}
+# print(type(dict))
+print(dir(dict))
