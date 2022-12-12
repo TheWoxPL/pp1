a60089bc21eaa4f18d39f392050207d6e9400223
s-121-28 <s-121-28@gumiforest.ae.krakow.pl>
2022-12-12 14:41:44 +0100

    :)

--- 10-ObjectOrientedProgramming/zad8.py
+++ 10-ObjectOrientedProgramming/zad8.py
@@ -1,6 +1,7 @@
 class TV:
     def __init__(self):
         self.is_on=False
+        self.channel_no=1
 
     def turn_on(self):
         self.is_on=True
@@ -8,15 +9,23 @@ class TV:
     def turn_off(self):
         self.is_on=False
 
+    def set_channel(self, channel):
+        self.channel_no=channel
+
     def show_status(self):
         if self.is_on:
-            print("Tv is on")
+            print(f"TV is on, channel {self.channel_no}")
         else:
-            print("Tv is off")
-            
+            print("TV is off")
+
         # print("Tv is on") if self.is_on else print("Tv is off")
 
 tv1=TV()
+tv1.show_status()
+tv1.turn_on()
+tv1.show_status()
+tv1.set_channel(5)
+tv1.show_status()
 tv1.turn_on()
 tv1.show_status()
         
